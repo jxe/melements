c87ac0f0fd55416da67f4fbba20576ee8806a3a7
Joe Edelman <joe.edelman@gmail.com>
2021-12-25 11:38:44 +0100

    fix overlay

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -2,6 +2,7 @@ import { styled, css } from '../stitches.config';
 
 export const overlayStyles = css({
   backgroundColor: 'rgba(0, 0, 0, .15)',
+  zIndex: '99',
 });
 
-export const Overlay = styled('div', overlayStyles);
+export const Overlay = styled('div', overlayStyles)
