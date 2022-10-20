437da191bf8be7e74060158be143d81d278fee4b
Joe Edelman <joe.edelman@gmail.com>
2022-10-20 11:14:24 +0200

    hmm

--- src/components/Feeler.tsx
+++ src/components/Feeler.tsx
@@ -1,5 +1,5 @@
 import { Cross1Icon, PlusIcon } from '@radix-ui/react-icons';
-import React, { useEffect, useState } from 'react';
+import React, { MouseEventHandler, useCallback, useEffect, useState } from 'react';
 import { isWhat } from '../emotions';
 import { BoldedList } from "./BoldedList";
 import { Button } from './Button';
@@ -7,38 +7,29 @@ import { Checkbox, CheckboxLabel } from './Checkbox';
 import { EmotionSingleSelect } from "./EmotionSelect";
 import { IconButton } from "./IconButton";
 import * as Multipane from "./Multipane";
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 // TODO
+// - "say more" button at the bottom goes to a second page which says "What connect is far away?" and lets you enter freeform and post, or make a values card
+// - start with scrollable box of basic feelings
+// - long-tap or force-touch to pick a more specific feeling
+
+// LATER
+// - emotions should start at 1, not 0
 // - Feeler should be its own route
 // - emotions colored
-// - "say more" button at the bottom goes to a second page which says "What connect is far away?" and lets you enter freeform and post, or make a values card
-// emotions should start at 1, not 0
 
-
-function EmotionCounter({ emotion, updated }: { emotion: string, updated: (emotion: string, count: number) => void }) {
-  const [count, setCount] = useState(0);
-  return <button
-    className='p-2 flex items-center justify-center rounded-sm border border-gray-300 active:bg-gray-100'
-    onClick={() => {
-      setCount(count + 1)
-      updated(emotion, count + 1)
-    }}
-  >
-    <div className='flex-auto text-left'>{emotion}</div>
-    <span className='text-slate-400'>{count}</span>
-  </button>
-}
 
 export function Feeler() {
   const [emotionCounts, setEmotionCounts] = useState<{ [emotion: string]: number }>({});
   const [activePane, setActivePane] = useState('feeler')
-  const [feelings, setFeelings] = useState<string[]>([])
   const [elapsed, setElapsed] = useState(0)
-  const onCancel = () => null
-  const onPost = () => null
-  function updateEmotionCount(emotion: string, count: number) {
-    setEmotionCounts({ ...emotionCounts, [emotion]: count })
-  }
+  const [parent] = useAutoAnimate<HTMLDivElement>({ disrespectUserMotionPreference: true, duration: 2000 })
+  const feelings = Object.keys(emotionCounts)
+  const incr: MouseEventHandler<HTMLButtonElement> = useCallback((event) => {
+    const emotion = event.currentTarget.id;
+    setEmotionCounts(emotionCounts => ({ ...emotionCounts, [emotion]: (emotionCounts[emotion] || 0) + 1 }))
+  }, [setEmotionCounts])
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -65,29 +56,30 @@ export function Feeler() {
   return (
     <Multipane.Root active={activePane}>
       <Multipane.Pane id="feeler">
-        <Multipane.Top
-          lButton={
-            <IconButton tabIndex={-1} variant="ghost" onClick={onCancel}>
-              <Cross1Icon />
-            </IconButton>
-          }
-          rButton={
-            <Button tabIndex={-1} onClick={onPost}>
-              Post
-            </Button>
-          }
-        >
+        <Multipane.Top>
           {minutes} min
         </Multipane.Top>
 
         <Multipane.PaneBody>
-          <div className='grid grid-cols-3 gap-2 flex-auto content-start items-baseline p-1'>
-            {feelingsOrdered.map(f => <EmotionCounter
-              key={f}
-              emotion={f}
-              updated={updateEmotionCount}
-            />)}
-            <EmotionSingleSelect onSelected={f => setFeelings([...feelings, f])}>
+          <div
+            id="feelenator"
+            key="feelenator"
+            className='grid grid-cols-3 gap-2 flex-auto content-start items-baseline p-1'
+            // className='content-start items-baseline p-1'
+            ref={parent}
+          >
+            {feelingsOrdered.map(f => (
+              <button
+                key={f}
+                id={f}
+                className='p-2 flex items-center justify-center rounded-sm bg-gray-100 active:bg-gray-200'
+                onClick={incr}
+              >
+                <div className='flex-auto text-left'>{f}</div>
+                <span className='text-slate-400'>{emotionCounts[f]}</span>
+              </button>
+            ))}
+            <EmotionSingleSelect onSelected={f => setEmotionCounts(counts => ({ ...counts, [f]: 1 }))}>
               <IconButton variant="ghost">
                 <PlusIcon />
               </IconButton>
@@ -97,8 +89,8 @@ export function Feeler() {
           {
             feelings.length > 0 &&
 
-            <div className='m-1 p-2 rounded-sm border-slate-400 border-solid border'>
-              <div className='text-center'>
+            <div className='m-1 p-2 rounded-sm bg-slate-100'>
+              <div className='text-center mb-3'>
                 What is <BoldedList or words={isWhatWordsOrdered.slice(0, 3)} />?
               </div>
               <div className='grid grid-cols-3 gap-2'>
